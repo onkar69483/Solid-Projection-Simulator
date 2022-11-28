351ba3b9b425e30d6f3953ee4b25cc45fe1aaa6f
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2022-11-29 00:12:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 import turtle
 import math
 
-                                                          # Inputting Values From User
-base_len = int(input("Enter Base length of hexagonal prism: "))
-axis_len = int(input("Enter Axis length of hexagonal prism: "))
-axis_angle = int(input("Enter Angle of Inclination of Hexagonal Prism: "))
-
 def x_axis(length):                                    #function used to draw x axis of a particular length
     c1 = turtle.Turtle()
+    c1.penup()
+    c1.setposition(inipos,0)
+    c1.pendown()
     c1.color("black")
     c1.speed(c1s)
     c1.left(45)
@@ -29,7 +27,7 @@ def x_axis(length):                                    #function used to draw x 
     x = turtle.Turtle()
     x.speed(xs)
     x.penup()
-    x.setposition(-15,-5)
+    x.setposition(inipos-15,-5)
     x.pendown()
     style = ('Arial', 10, 'italic')
     x.write('x', font=style, align='center')
@@ -38,17 +36,23 @@ def x_axis(length):                                    #function used to draw x 
     y = turtle.Turtle()
     y.speed(ys)
     y.penup()
-    y.setposition(length+15,-5)
+    y.setposition(length+15+inipos,-5)
     y.pendown()
     style = ('Arial', 10, 'italic')
     y.write('y', font=style, align='center')
     y.hideturtle()
 
-def hexagon(base_len1, axis_len1, axis_angle1):                  # Hexagon function to create a hexagon
+def hexagonalprism(base_len1, axis_len1, axis_angle1):                  # Hexagon function to create a hexagon
+    alpha.setposition(200,300)
+    alpha.pendown()
+    style = ('Arial', 20, 'underline')
+    alpha.write("Projection Of Solids (Hexagonal Prism)", font=style, align='right')  # 61
+    alpha.penup()
+
     hex = turtle.Turtle()
     hex.speed(hexs)
     hex.penup()
-    hex.setposition(30, -((base_len1*1.732/2)+30))
+    hex.setposition(inipos+30, -((base_len1*1.732/2)+30))
     hex.left(60)
     hex.pendown()
     lightl = turtle.Turtle()
@@ -173,7 +177,7 @@ def hexagon(base_len1, axis_len1, axis_angle1):                  # Hexagon funct
         hex.right(60)
 
     light3.hideturtle()
-    light2.setposition(light2.xcor()+50,light2.ycor())     #moves 50 ahead of the axis rectangle
+    light2.setposition(light2.xcor()+80,light2.ycor())     #moves 50 ahead of the axis rectangle
     l2pos = light2.position()
     l2posx = light2.xcor()
     light2.showturtle()
@@ -629,35 +633,493 @@ def hexagon(base_len1, axis_len1, axis_angle1):                  # Hexagon funct
     alpha.write("61", font=style, align='right')      # 61
     alpha.penup()
 
+def hexagonalpyramid(base_len1, axis_len1, axis_angle1): # function to make projection of hexagonal pyramid
     alpha.setposition(200,300)
-    alpha.pendown()
     style = ('Arial', 20, 'underline')
-    alpha.write("Projection Of Solids (Hexagonal Prism)", font=style, align='right')  # 61
+    alpha.write("Projection of solids (Hexagonal Pyramid)", font = style, align='right')
+    alpha.penup()
+    hex = turtle.Turtle()
+    hex.speed(hexs)
+    hex.penup()
+    hex.setposition(inipos+70, 0)
+    hex.left(120)
+    hex.pendown()
+    for i in range(6):                                                  #drawing hexagon
+        hex.forward(base_len1)
+        hex.right(60)
+    hex.penup()
+    hex.hideturtle()
+    lightl = turtle.Turtle()
+    light2 = turtle.Turtle()
+    light2.hideturtle()
+    light2.penup()
+    lightl.penup()
+    lightl.setposition(hex.position())
+    lightl.pendown()
+    for j in range(6):                                                 #drawing light lines1
+        lightl.color("grey")
+        lightl.setposition(hex.position())
+        lightl.pendown()
+        lightl.setposition(hex.xcor(), 0)
+        lightl.penup()
+        hex.forward(base_len1)
+        hex.right(60)
+
+    lightl.hideturtle()
+    for i in range(6):
+        if i==1:
+            light2.setposition(hex.xcor(),-50)
+            rec1 = light2.position()
+            rec11 = light2.xcor()
+            hex2= hex.position()
+            hex22 = hex.ycor()
+            fdx = hex.xcor()
+            fdy = hex.ycor()
+            fx = hex.xcor()
+            fy = -50
+        if i==4:
+            light2.setposition(hex.xcor(),-50)
+            rec2 = light2.position()
+            rec22 = light2.xcor()
+            cdx = hex.xcor()
+            cdy = hex.ycor()
+            cx = hex.xcor()
+            cy = -50
+        if i==2:
+            rec4=hex.ycor()
+            rec6=hex.xcor()
+            hex1 = hex.position()
+            hex11 = hex.ycor()
+            edx = hex.xcor()
+            edy = hex.ycor()
+            ex = hex.xcor()
+            ey = -50
+        if i==3:
+            rec7=hex.xcor()
+            ddx = hex.xcor()
+            ddy = hex.ycor()
+            dx = hex.xcor()
+            dy = -50
+        if i==0:
+            hex3 = hex.position()
+            hex33 = light2.ycor()
+            adx = hex.xcor()
+            ady = hex.ycor()
+            ax = hex.xcor()
+            ay = -50
+        if i==5:
+            bdx = hex.xcor()
+            bdy = hex.ycor()
+            bx = hex.xcor()
+            by = -50
+        hex.forward(base_len1)
+        hex.right(60)
+    light2.setposition(rec1)
+    light2.pendown()
+    light2.setposition(rec2)
+    light2.penup()
+    for j in range(6):                                                 #drawing light lines1
+        hex.forward(base_len1)
+        hex.right(60)
+        lightl.color("grey")
+        lightl.setposition(hex.position())
+        lightl.pendown()
+        lightl.setposition(hex.xcor(), -50)
+        lightl.penup()
+    rec3 = rec22-rec11
+    rec5 = rec11+rec3/2
+    light2.setposition(rec11+rec3/2,rec4)
+    light2.setheading(-90)
+    newv = math.ceil((int(rec4)+50+axis_len1)/25)
+    light2.speed(100)
+    for i in range(newv):
+        if(light2.ycor()==-(50+axis_len1)):
+            break
+        else:
+            light2.pendown()
+            light2.forward(10)
+            light2.penup()
+            light2.forward(5)
+            light2.pendown()
+            light2.circle(0.5)
+            light2.penup()
+            light2.forward(10)
+    oy = -(50+axis_len1)
+    print(light2.position())
+    light2.setposition(rec1)
+    light2.pendown()
+    light2.setposition(rec5,-(50+axis_len1))
+    light2.penup()
+    light2.setposition(rec2)
+    light2.pendown()
+    light2.setposition(rec5, -(50 + axis_len1))
+    light2.penup()
+    light2.color("grey")
+    light2.setposition(rec6,-50)
+    light2.pendown()
+    light2.setposition(rec5, -(50 + axis_len1))
+    light2.penup()
+    light2.setposition(rec7,-50)
+    light2.pendown()
+    light2.setposition(rec5, -(50 + axis_len1))
+    light2.penup()
+
+    light2.setposition(rec22+80,-1)     # check this statement again
+    l2pos = light2.position()
+    thetax = light2.xcor()
+    thetay = light2.ycor()
+    light2.setheading(0)
+    light2.right(axis_angle1)
+    light2.color("black")
+    rec8 = rec3 / 2
+    newv1=math.ceil((axis_len1+50)/24)
+    light2.showturtle()
+    for i in range(newv1):
+
+        light2.pendown()
+        light2.forward(10)
+        light2.penup()
+        light2.forward(5)
+        light2.pendown()
+        light2.dot(3)
+        light2.penup()
+        light2.forward(10)
+    light2.setposition(l2pos)
+    light2.speed(100)
+    light2.forward(0.1)
+    for i in range(1000):
+        light2.forward(1)
+        light2.left(90)
+        light2.forward(rec8)
+
+        if math.floor(light2.ycor())==0:
+            rec12 = light2.xcor()
+            light2.backward(rec8)
+            rec9=light2.position()
+            light2.right(90)
+            light2.forward(axis_len1)
+            rec10=light2.position()
+            o1x=o1dx= light2.xcor()
+            o1y = light2.ycor()
+            o1dy = hex22
+            rec23 = light2.xcor()
+            light2.backward(axis_len1)
+            light2.left(90)
+            light2.forward(rec8)
+            light2.pendown()
+            light2.backward(rec3)
+            rec15 = light2.position()
+            rec17=light2.xcor()
+            f1x =f1dx= light2.xcor()
+            f1y = light2.ycor()
+            f1dy=hex22
+            light2.setposition(rec10)
+            light2.penup()
+            light2.setposition(rec12,0)
+            light2.pendown()
+            light2.setposition(rec10)
+            light2.penup()
+            break
+        else:
+            light2.backward(rec8)
+            light2.right(90)
+    light2.color("grey")
+    rec13 = rec6-rec11
+    rec14 = rec7-rec11
+    light2.setposition(rec15)
+    light2.forward(rec13)
+    rec18 = light2.position()
+    rec19 = light2.xcor()
+    a1x=e1x=a1dx=e1dx=light2.xcor()
+    a1y=e1y=light2.ycor()
+    a1dy=hex33
+    e1dy=hex11
+    light2.pendown()
+    light2.setposition(rec10)
+    light2.penup()
+    light2.setposition(rec15)
+    light2.forward(rec14)
+    rec20 = light2.position()
+    rec21 = light2.xcor()
+    b1x=d1x=b1dx=d1dx= light2.xcor()
+    b1y=d1y = light2.ycor()
+    b1dy = hex33
+    d1dy = hex11
+    light2.pendown()
+    light2.setposition(rec10)
+    light2.penup()
+    light2.setposition(hex1)
+    light2.setheading(0)
+    light2.pendown()
+    light2.forward(len2)
+    rec16=light2.xcor()
+    light2.penup()
+    c1x=c1dx=rec12
+    c1y=0
+    c1dy=hex22
+    light2.setposition(hex2)
+    light2.pendown()
+    light2.setposition(rec16,hex22)
+    light2.penup()
+    light2.setposition(rec15)
+    light2.setheading(90)
+    light2.pendown()
+    light2.setposition(rec17,hex11)
+    light2.penup()
+    light2.setposition(rec18)
+    light2.setheading(90)
+    light2.pendown()
+    light2.setposition(rec19,hex11)
+    light2.penup()
+    light2.setposition(rec20)
+    light2.setheading(90)
+    light2.pendown()
+    light2.setposition(rec21,hex11)
+    light2.penup()
+    light2.setposition(rec12,0)
+    light2.setheading(90)
+    light2.pendown()
+    light2.setposition(rec12,hex11)
+    light2.penup()
+    light2.setposition(rec10)
+    light2.setheading(90)
+    light2.pendown()
+    light2.setposition(rec23,hex11)
+    light2.penup()
+
+    light2.color("black")
+    light2.setposition(rec21,hex33)
+    light2.pendown()
+    light2.setposition(rec19,hex33)
+    light2.setposition(rec17,hex22)
+    light2.setposition(rec19,hex11)
+    light2.setposition(rec21,hex11)
+    light2.color("red")
+    light2.setposition(rec12,hex22)
+    light2.setposition(rec21,hex33)
+
+    light2.color("black")
+    light2.setposition(rec23,hex22)
+    light2.penup()
+    light2.setposition(rec21,hex11)
+    light2.pendown()
+    light2.setposition(rec23,hex22)
+    light2.penup()
+    light2.color("grey")
+    light2.setposition(rec19,hex33)
+    light2.pendown()
+    light2.setposition(rec23,hex22)
+    light2.penup()
+    light2.setposition(rec19,hex11)
+    light2.pendown()
+    light2.setposition(rec23,hex22)
+    light2.penup()
+    light2.hideturtle()
+
+    #Naming
+    alpha.setposition(adx,ady-12)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("a'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(bdx,bdy-12)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("b'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(cdx+8,cdy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("c'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(ddx,ddy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("d'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(edx,edy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("e'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(fdx,fdy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("f'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(rec5-3,hex22)
+    alpha.pendown()
+    style = ('Arial', 8)
+    alpha.write("o'", font=style, align='right')
     alpha.penup()
 
-                                                                #Turtle speeds
-alpha = turtle.Turtle()
-alpha.hideturtle()
-alpha.penup()
-alphas=1
-alpha.speed(alphas)
-speeds=2
-c1s=speeds
-xs=speeds
-ys=speeds
-hexs=speeds
-light1s=speeds
-light2s=speeds
-light3s=speeds
+    alpha.setposition(ax,ay-20)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("a", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(bx,by-20)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("b", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(cx,cy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("c", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(dx,dy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("d", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(ex,ey)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("e", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(fx,fy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("f", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(rec5,oy-15)
+    alpha.pendown()
+    style = ('Arial', 9)
+    alpha.write("o", font=style, align='right')
+    alpha.penup()
 
-                                                      #forward check distance
-fcheck=1
+    alpha.setposition(a1x+3,a1y-20)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("a1", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(b1x+3,b1y-20)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("b1", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(c1x+15,c1y)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("c1", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(d1x,d1y-5)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("d1", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(e1x,e1y-5)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("e1", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(f1x,f1y)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("f1", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(o1x+10,o1y-15)
+    alpha.pendown()
+    style = ('Arial', 8)
+    alpha.write("o1", font=style, align='right')
+    alpha.penup()
 
-len2=base_len*10                                      #light lines length in x direction
-lenl = base_len*10                                    #x-axis length
+    alpha.setposition(a1dx-5,a1dy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("a1'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(b1dx,b1dy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("b1'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(c1dx+15,c1dy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("c1'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(d1dx,d1dy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("d1'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(e1dx,e1dy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("e1'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(f1dx,f1dy)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("f1'", font=style, align='right')
+    alpha.penup()
+    alpha.setposition(o1dx+20,o1dy)
+    alpha.pendown()
+    style = ('Arial', 8)
+    alpha.write("o1'", font=style, align='right')
+    alpha.penup()
 
-x_axis(lenl)                               #function to draw x axis of a particular length
+    alpha.setposition(thetax+30,thetay-12)
+    alpha.pendown()
+    style = ('Arial', 7)
+    alpha.write("θ="+str(axis_angle1), font=style, align='right')
+    alpha.penup()
 
-hexagon(base_len,axis_len,axis_angle)      #function to draw hexagon by inputting specific parameters
+
+inipos=-500
+shape = input("Enter solid name: ")  # Inputting the type of solid from the user
+
+if (shape == "hexagonal prism"):
+    base_len = int(input("Enter Base length of hexagonal prism: ")) #inputting values of hexagonal prism from user
+    axis_len = int(input("Enter Axis length of hexagonal prism: "))
+    axis_angle = int(input("Enter Angle of Inclination of Hexagonal Prism: "))
+    # Turtle speeds
+    alpha = turtle.Turtle()
+    alpha.hideturtle()
+    alpha.penup()
+    alphas = 10
+    alpha.speed(alphas)
+    speeds = 10
+    c1s = speeds
+    xs = speeds
+    ys = speeds
+    hexs = speeds
+    light1s = speeds
+    light2s = speeds
+    light3s = speeds
+    #forward check distance
+    fcheck=1
+    len2=base_len*5                                       #light lines length in x direction
+    lenl = base_len*10                                    #x-axis length
+    x_axis(lenl)                                          #function to draw x axis of a particular length
+    hexagonalprism(base_len,axis_len,axis_angle)          #function to draw hexagonal prism by inputting specific parameters
+elif (shape=="hexagonal pyramid"):
+    base_len = int(input("Enter Base length of hexagonal pyramid: ")) #inputting values of hexagonal prism from user
+    axis_len = int(input("Enter Axis length of hexagonal pyramid: "))
+    axis_angle = int(input("Enter Angle of Inclination of Hexagonal pyramid: "))
+    # Turtle speeds
+    alpha = turtle.Turtle()
+    alpha.hideturtle()
+    alpha.penup()
+    alphas = 10
+    alpha.speed(alphas)
+    speeds = 10
+    c1s = speeds
+    xs = speeds
+    ys = speeds
+    hexs = speeds
+    light1s = speeds
+    light2s = speeds
+    light3s = speeds
+    #forward check distance
+    fcheck=1
+    len2=base_len*9                                          #light lines length in x direction
+    lenl = base_len * 10
+    x_axis(lenl)                                                #function to draw x axis of a particular length
+    hexagonalpyramid(base_len,axis_len,axis_angle)              #function to draw hexagonal pyramid by inputting specific parameters
+else:
+    print("\nInvalid shape!")
 
 turtle.done()
